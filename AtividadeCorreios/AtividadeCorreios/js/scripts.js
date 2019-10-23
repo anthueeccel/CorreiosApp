7489148fcc2cd24a3b88b20df69daa3f0b9f9172
Jean Prim <gangrelrox@hotmail.com>
2019-10-23 08:42:26 -0300

    2nd index n script - paralelo

---
+++ AtividadeCorreios/AtividadeCorreios/js/scripts.js
@@ -0,0 +1,13 @@
+﻿$(document).ready(function () {
+    $("#cepOrigem").mask("99999-999");
+    $("#cepDestino").mask("99999-999");
+});
+function LimparTela() {
+    var meusInputs = $('input[type="text"]');
+    meusInputs.val("");
+}
+$(document).on('keypress', function (e) {
+    if (e.which == 13) {
+        $('input[type="button"]').click();
+    }
+});
